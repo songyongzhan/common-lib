cd2368a04e88d6f6322c9072441027390eb3395e
songyz <574482856@qq.com>
2020-05-20 22:46:11 +0800

    扩展Tree工具包

--- src/Library/Tree.php
+++ src/Library/Tree.php
@@ -11,6 +11,19 @@ namespace Songyz\Common\Library;
  */
 class Tree
 {
+    /**
+     * 以分组的形式展示
+     * menuGroupList
+     * @param $data
+     * @param int $pid
+     * @param string $primaryKey
+     * @param string $parent_name
+     * @param int $step
+     * @return array
+     *
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 21:58
+     */
     public static function menuGroupList($data, $pid = 0, $primaryKey = 'id', $parent_name = 'pid', $sun_name = 'sun')
     {
         if (empty($data)) {
@@ -27,6 +40,19 @@ class Tree
         return $result;
     }
 
+    /**
+     * 按照step显示栏目
+     * menuList
+     * @param $data
+     * @param int $pid
+     * @param string $primaryKey
+     * @param string $parent_name
+     * @param int $step
+     * @return array
+     *
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 21:58
+     */
     public static function menuList($data, $pid = 0, $primaryKey = 'id', $parent_name = 'pid', $step = 0)
     {
         if (empty($data)) {
@@ -38,14 +64,14 @@ class Tree
                 $val['step'] = $step;
                 $result[] = $val;
                 $result = array_merge($result,
-                    self::menuList($result, $val[$primaryKey], $primaryKey, $parent_name, $step + 1));
+                    self::menuList($data, $val[$primaryKey], $primaryKey, $parent_name, $step + 1));
             }
         }
         return $result;
     }
 
     /**
-     *
+     * 子类获取父级所有栏目
      * getParent
      * @param $data
      * @param $currentId
@@ -53,7 +79,8 @@ class Tree
      * @param string $parentName
      * @return array
      *
-     * @date 2020/5/20 13:59
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 21:58
      */
     public static function getParent($data, $currentId, $primaryKey = 'id', $parentName = 'pid')
     {
@@ -71,7 +98,18 @@ class Tree
         return $result;
     }
 
-    public static function menuSortBySortId($data, $sortFields = 'sort_id', $sortType = 'asc')
+    /**
+     * 按照字段排序
+     * menuSortBy
+     * @param $data
+     * @param string $sortFields
+     * @param string $sortType
+     * @return mixed
+     *
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 21:57
+     */
+    public static function menuSortBy($data, $sortFields = 'sort_id', $sortType = 'asc')
     {
         if ($sortType == 'asc') {
             usort($data, function ($a, $b) use ($sortFields) {
@@ -90,6 +128,58 @@ class Tree
         }
 
         return $data;
+    }
+
+
+    /**
+     * 根据pid获取到子类所有id
+     * getSunIds
+     * @param array $data
+     * @param string $pid
+     * @param string $pidField
+     * @param string $idField
+     * @return array
+     *
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 22:06
+     */
+    public static function getSunIds(array $data, string $pid, string $pidField = 'pid', string $idField = 'id')
+    {
+        $result = [];
+        foreach ($data as $key => $val) {
+            if ($val[$pidField] == $pid) {
+                $result[] = $val[$idField];
+                $result = array_merge(self::getSunIds($data, $val[$idField], $pidField, $idField), $result);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * 获取子类列表
+     * getSuns
+     * @param array $data
+     * @param string $pid
+     * @param string $pidField
+     * @param string $idField
+     * @return array
+     *
+     * @author songyz <574482856@qq.com>
+     * @date 2020/5/20 22:35
+     */
+    public static function getSuns(array $data, string $pid, $pidField = 'pid', $idField = 'id')
+    {
+        $result = [];
+        foreach ($data as $key => $val) {
+            if ($val[$pidField] == $pid) {
+                $result[] = $val;
+                $result = array_merge($result,
+                    self::getSuns($data, $val[$idField], $pidField, $idField));
+            }
+        }
+
+        return $result;
     }
 
 
--- tests/Jwt/TreeTest.php
+++ tests/Jwt/TreeTest.php
@@ -8,13 +8,63 @@ use Songyz\Common\Library\Tree;
 
 class TreeTest extends TestCase
 {
+    private $data = [
+        ['id' => 1, 'title' => '苹果', 'pid' => 0, 'sort_id' => 2],
+        ['id' => 2, 'title' => '小米', 'pid' => 0, 'sort_id' => 1],
+        ['id' => 3, 'title' => '华为', 'pid' => 0, 'sort_id' => 3],
+        ['id' => 4, 'title' => 'iphone6', 'pid' => 1, 'sort_id' => 2],
+        ['id' => 11, 'title' => 'iphone6 plus', 'pid' => 4, 'sort_id' => 2],
+        ['id' => 5, 'title' => 'iphone7', 'pid' => 1, 'sort_id' => 3],
+        ['id' => 6, 'title' => 'iphone5', 'pid' => 1, 'sort_id' => 1],
+        ['id' => 7, 'title' => 'iphone9', 'pid' => 1, 'sort_id' => 4],
+        ['id' => 8, 'title' => '小米', 'pid' => 2, 'sort_id' => 1],
+        ['id' => 9, 'title' => '红米', 'pid' => 2, 'sort_id' => 2],
+        ['id' => 10, 'title' => 'nova', 'pid' => 3, 'sort_id' => 0],
+    ];
 
-    public function testMenuGroupList(){
 
-        Tree::menuGroupList();
+    public function testGetSuns(){
+        $result = Tree::getSuns($this->data,'1');
+        $result = Tree::menuSortBy($result, 'sort_id', 'asc');
 
-
+        print_r($result);
+        $this->assertIsBool(true);
     }
 
+    public function testGetSunIds(){
+        $result = Tree::getSunIds($this->data,'1');
+
+        print_r($result);
+        $this->assertIsBool(true);
+    }
+
+    public function testMenuGroupList()
+    {
+        $data = $this->data;
+        $data = Tree::menuSortBy($data, 'sort_id', 'asc');
+
+        $result = Tree::menuGroupList($data);
+
+        print_r($result);
+        $this->assertIsBool(true);
+    }
+
+    public function testMenuList()
+    {
+        $result = Tree::menuList($this->data);
+
+        print_r($result);
+        $this->assertIsBool(true);
+    }
+
+
+    public function testGetParent()
+    {
+        $result = Tree::getParent($this->data, 11);
+        $result = array_reverse($result);
+
+        print_r($result);
+        $this->assertIsBool(true);
+    }
 
 }
